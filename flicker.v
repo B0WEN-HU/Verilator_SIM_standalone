124dfdd2b1ec0700c9b67b902a77d1d965719224
Bowen Hu <46917802+B0WEN-HU@users.noreply.github.com>
2019-05-11 23:35:55 +0800

    Format the code

--- flicker.v
+++ flicker.v
@@ -1,12 +1,12 @@
-module flicker(clk,rst_n,dout);
+module flicker(clk, rst_n, dout);
 parameter WIDTH = 5;
-input clk,rst_n;
+input clk, rst_n;
 output reg [WIDTH-1:0] dout;
 
 always@(posedge clk or negedge rst_n)
 
     if(!rst_n)
-        dout <= {WIDTH{1'b0}};
+        dout <= { WIDTH{ 1'b0 } };
     else
         dout <= ~dout;
 
